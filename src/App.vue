23b31dff22b3b0b71e2170cc84084975f7da00b5
Antonio Molinari <molinari@incode.it>
2025-04-09 13:15:33 +0200

    fixed tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import SplashScreen from './components/SplashScreen.vue'
 </script>
 
 <template>
-  <div class="flex justify-center items-center gap-8 mb-8">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="h-24 p-6 transition-all duration-300 hover:drop-shadow-lg" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="h-24 p-6 transition-all duration-300 hover:drop-shadow-lg" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <SplashScreen />
 </template>
